be4bedacaa084bd91f196670e667df93e34b25d2
Devsstudent <devsstudent777@gmail.com>
2022-11-24 11:58:57 +0100

    libft + struct header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: odessein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 10:39:07 by odessein          #+#    #+#             */
-/*   Updated: 2022/11/24 10:46:28 by odessein         ###   ########.fr       */
+/*   Updated: 2022/11/24 11:52:01 by odessein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef CUB3D_H
@@ -17,5 +17,38 @@
 # include <math.h>
 # include <string.h>
 # include <stdbool.h>
+
+typedef struct	s_data{
+	int		floor_color;
+	int		ceilling_color;
+	char	*east_texture;
+	char	*north_texture;
+	char	*west_texture;
+	char	*south_texture;
+}			t_data;
+
+typedef struct	s_player{
+	bool	up;
+	bool	down;
+	bool	left;
+	bool	right;
+	int		x;
+	int		y;
+}			t_player;
+
+typedef struct	s_vision{
+	int		fov;
+	bool	up;
+	bool	down;
+	bool	left;
+	bool	right;
+	int		horizontal_angle;
+}			t_vision;
+
+typedef struct	s_game_info{
+	t_player	player;
+	t_vison		camera;
+	t_data		data;
+}
 
 #endif
