690bf84290ff55ed540781a180f81bcf8a0e9329
Devsstudent <devsstudent777@gmail.com>
2022-11-24 09:54:28 +0100

    idea

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 
 int	main(int ac, char *av)
 {
+	//Structure definir sur la stack
 	if (ac != 2)
 		return (1);
+	//Parsing
+		//if not error msg (function for it)
+	//Start_game
 }
+
+
+//arg_1 : structure data (a definir)
+//arg_2 : structure game_info (a defnir) -> struct player -> struct camera etc
+//
+bool	start_game()
